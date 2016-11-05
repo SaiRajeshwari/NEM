9c33522ed57d40fb8cbdcc520373c7ddb58e7239
Sai Rajeshwari <sairajeshwari.g@gmail.com>
2016-11-05 15:39:41 -0700

    Use __dirname and sendFile

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.listen(3000, function(){
 	console.log('listening on 3000')
 })
 
-app.get('/', function(req, res){
-	res.send('Hello World')
+app.get('/', (req, res) => {
+	//res.send('Hello World')
+	res.sendFile(__dirname + '/index.html')
+	console.log('__dirname : ' + __dirname)
 })
